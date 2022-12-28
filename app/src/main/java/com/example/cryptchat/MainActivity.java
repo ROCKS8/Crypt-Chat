8cd53f346d3d411a601d5df3b27c6c7e1621ea6e
Varshil Patel <patel.varshil1850@gmail.com>
2022-12-28 12:45:50 +0530

    Crypt Chat

---
+++ app/src/main/java/com/example/cryptchat/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.cryptchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    MotionLayout motionLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        MotionLayout motionLayout = findViewById(R.id.motionLayout);
+        motionLayout.startLayoutAnimation();
+        motionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
+
+            }
+
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
+
+            }
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
+
+//                Toast.makeText(MainActivity.this, "Helllo", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/cryptchat/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.cryptchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        System.out.println("************************************************");
+        System.out.println("On the Activity 2");
+        System.out.println("************************************************");
+
+    }
+}
